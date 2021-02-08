166d5550396a64d43f7f79671f9b40c8aa806871
Dedigraz <ebunrereadebanjo@outlook.com>
2021-02-08 05:13:52 +0100

    A6

---
+++ Assets/InventoryUI.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+	public Transform itemsPanel;
+	InventorySlot[] slots;
+	Inventory inventory;
+	// Start is called before the first frame update
+	void Start()
+	{
+		inventory = Inventory.instance;
+		inventory.onItemChangedCallback += UpdateUI;
+
+		slots = itemsPanel.GetComponentsInChildren<InventorySlot>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+	void UpdateUI()
+	{
+		for (int i = 0; i < slots.Length; i++)
+		{
+			if (i < inventory.Items.Count)
+			{
+				slots[i].AddItem(inventory.Items[i]);
+			}
+			else
+			{
+				slots[i].ClearSlot();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Store Items",menuName ="Inventory/Store Item")]
+public class Item : ScriptableObject
+{
+	new public string name = "New Item";
+	public Sprite icon = null;
+	public bool isDefaultItem = false;
+}
