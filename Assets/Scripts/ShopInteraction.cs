95216b2e553921023c4566ac4f76dd0300c52ae1
Dedigraz <ebunrereadebanjo@outlook.com>
2021-02-09 16:11:11 +0100

    A9

--- Assets/Scripts/ShopInteraction.cs
+++ Assets/Scripts/ShopInteraction.cs
@@ -6,13 +6,19 @@ using UnityEngine.UI;
 public class ShopInteraction : MonoBehaviour
 {
 	public Animator animator;
-	public Wallet Wallet;
-	public Text wallet_Balance;
+	[SerializeField] public Text wallet_Balance;
+	[SerializeField]private Item item1;
+	[SerializeField]private Item item2;
 
-	private void Start()
+
+	//private void Start()
+	//{
+	//	Wallet.Instance.GetBalance();
+	//}
+
+	private void Refresh()
 	{
-		Wallet = Wallet.Instance;
-		wallet_Balance.text = Wallet.money.ToString();
+		wallet_Balance.text = Wallet.Instance.GetBalance().ToString(); //Wallet.GetBalance().ToString("N0");
 	}
 	public void OpenShop()
 	{
@@ -23,5 +29,19 @@ public class ShopInteraction : MonoBehaviour
 	public void CloseShop()
 	{
 		animator.SetBool("IsRedPill", false);
+	}
+
+	public void BuyItem1()
+	{
+		Wallet.Instance.TryWithdraw(item1.cost);
+		Inventory.instance.Add(item1);
+		CloseShop();
+	}
+
+	public void BuyItem2()
+	{
+		Wallet.Instance.TryWithdraw(item1.cost);
+		Inventory.instance.Add(item2);
+		CloseShop();
 	}
 }
